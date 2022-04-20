5c2304f0cc47ac6fb14aa2ce8f3d0adac5b09367
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-04-20 19:22:41 +0300

    change style of "not found" image

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -74,10 +74,22 @@ class RandomChar extends Component{
 }
 
 const View = ({char}) => {
+
     const {name, description, homepage, wiki, thumbnail} = char;
+
+    const errorImg = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
+    let styleImg;
+    
+    if (errorImg === thumbnail) {
+        styleImg = 'contain'
+    }
+    else {
+        styleImg = 'cover'
+    }
+
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={{objectFit: styleImg}}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
